bbfb58c932a237cee39a895e2f319da6052e5321
Alissandra <alissandraraysa@gmail.com>
2022-05-27 15:17:04 -0300

    estilizacao da pagina de projetos front end

--- src/pages/ProjectsFront.js
+++ src/pages/ProjectsFront.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Projects.css';
+import './ProjectsFront.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { dataFrontEnd } from '../services/dataFrontEnd';
@@ -8,21 +8,27 @@ function Projects() {
   return (
     <div>
       <Header />
-      <h1>Projetos</h1>
-      <h2>Os projetos estão organizados em ordem cronológica, iniciando do mais recente:</h2>
+      <div className="cotainer-margin">
+      <h1 className="title">Projetos</h1>
+      <h2 className="subtitle">Os projetos estão organizados em ordem cronológica, iniciando do mais recente:</h2>
+      <div className="spacing-projects-div">
       {dataFrontEnd.map((p) => (
-      <div key={p.name}>
+      <div key={p.name} className="projects-front">
         <a href={p.url}>
-          <div className="container-project">
-          <img src={ p.image } alt={ p.name } />
+          <div className="box-project-front">
+          <img src={ p.image } alt={ p.name } className="img-front"/>
+           <div className="div-text-front">
             <h4>O que é</h4>
             <p>{p.name}</p>
             <p>{p.description}</p>
+           </div>
           </div>
         </a>
         </div>
       ))}
-      <Footer />
+      </div>
+      </div>
+      <Footer className="footer-front"/>
     </div>
   )
 }
